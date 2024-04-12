a69f85cd3e19ecd4717b984a281af600e7a8a3e4
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-04-12 02:40:25 -0300

    :sparkles: feat: Implementação da aplicação 5

---
+++ aplicacao5.ts
@@ -0,0 +1,22 @@
+// Importando as classes necessárias
+import Cliente from './src/models/Cliente';
+import ContaCorrente from './src/models/ContaCorrente';
+
+// Criando um cliente com uma ContaCorrente
+const cliente1 = new Cliente('12345678900', 'Fulano', '123456789', false);
+const contaCorrente1 = new ContaCorrente('1111', 100); // Definindo o limite como 100 reais
+cliente1.adicionarConta(contaCorrente1);
+
+// Efetuando um depósito de 300 na ContaCorrente
+contaCorrente1.depositar(300);
+
+// Criando outro cliente com outra ContaCorrente
+const cliente2 = new Cliente('98765432100', 'Ciclano', '987654321', false);
+const contaCorrente2 = new ContaCorrente('2222', 0); // Sem limite
+cliente2.adicionarConta(contaCorrente2);
+
+// Efetuando um depósito de 100 na segunda ContaCorrente
+contaCorrente2.depositar(100);
+
+// Tentando efetuar uma transferência de 1000 da primeira ContaCorrente para a segunda
+contaCorrente1.transferir(contaCorrente2, 1000);
